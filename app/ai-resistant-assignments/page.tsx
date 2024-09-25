3973625f9d391b0daf111c7620184ae68df404f9
WilfredoAaronSosaRamosBoushtech <U20204320@gmail.com>
2024-09-25 09:29:53 -0500

    feature/ai-resistant-assignments

---
+++ app/ai-resistant-assignments/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import AIResistantForm from "@/components/ai_resistant_asignments/AIResistantAssignmentsForm";
+import Layout from "@/components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <AIResistantForm />
+    </Layout>
+  );
+}
