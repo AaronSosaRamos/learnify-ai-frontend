671ac27e5a1e70a803db09690a01c79267601613
WilfredoAaronSosaRamosBoushtech <U20204320@gmail.com>
2024-09-25 11:10:01 -0500

    feature/ai-rubric-generator

---
+++ app/rubric-generator/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import AIRubricGeneratorForm from "@/components/ai_rubric_generator/AIRubricGenerator";
+import Layout from "@/components/Layout";
+
+export default function PersonalizedTasks() {
+  return (
+    <Layout>
+      <AIRubricGeneratorForm />
+    </Layout>
+  );
+}
